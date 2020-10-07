103e877ac21cf90d482cabdd87fea91b876ad266
Charles Simpson <champs@bas.ac.uk>
2020-10-07 16:07:10 +0100

    script for getting cmip6 data from esgf

---
+++ src/get_data/esgf_opendap.py
@@ -0,0 +1,65 @@
+import warnings
+import pandas as pd
+import requests
+
+# Below taken from https://hub.binder.pangeo.io/user/pangeo-data-pan--cmip6-examples-ro965nih/lab
+def esgf_search(server="https://esgf-node.llnl.gov/esg-search/search",
+                files_type="OPENDAP", local_node=True, project="CMIP6",
+                verbose=False, format="application%2Fsolr%2Bjson",
+                use_csrf=False, **search):
+    client = requests.session()
+    payload = search
+    payload["project"] = project
+    payload["type"]= "File"
+    if local_node:
+        payload["distrib"] = "false"
+    if use_csrf:
+        client.get(server)
+        if 'csrftoken' in client.cookies:
+            # Django 1.6 and up
+            csrftoken = client.cookies['csrftoken']
+        else:
+            # older versions
+            csrftoken = client.cookies['csrf']
+        payload["csrfmiddlewaretoken"] = csrftoken
+
+    payload["format"] = format
+
+    offset = 0
+    numFound = 10000
+    all_files = []
+    files_type = files_type.upper()
+    while offset < numFound:
+        payload["offset"] = offset
+        url_keys = []
+        for k in payload:
+            url_keys += ["{}={}".format(k, payload[k])]
+
+        url = "{}/?{}".format(server, "&".join(url_keys))
+        print(url)
+        r = client.get(url)
+        r.raise_for_status()
+        resp = r.json()["response"]
+        numFound = int(resp["numFound"])
+        resp = resp["docs"]
+        offset += len(resp)
+        for d in resp:
+            if verbose:
+                for k in d:
+                    print("{}: {}".format(k,d[k]))
+            url = d["url"]
+            for f in d["url"]:
+                sp = f.split("|")
+                if sp[-1] == files_type:
+                    all_files.append(sp[0].split(".html")[0])
+    return sorted(all_files)
+
+
+example_search = {
+    'variable_id': 'tas',
+    'table_id': 'Amon',
+    'activity_id': 'CMIP',
+    'source_id': 'HadGEM3-GC31-MM',
+    'member_id': 'r1i1p1f1',
+    'experiment_id': 'historical'
+}
