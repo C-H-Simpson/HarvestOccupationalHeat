db3ef5db50d8d7243b4f06b612014485e55186ad
Charles Simpson <champs@bas.ac.uk>
2020-10-29 16:56:00 +0000

    remove check that openID is not already set

--- openID.sh
+++ openID.sh
@@ -1,11 +1,8 @@
-if [ -z "${openID}" ]
-then
-    echo "Enter openID"
-    read openID
-    export openID=$openID
+echo "Enter openID"
+read openID
+export openID=$openID
 
-    echo "Enter openID password"
-    echo "This will be stored as an environment variable"
-    read -s openID_password
-    export openID_password=$openID_password
-fi
+echo "Enter openID password"
+echo "This will be stored as an environment variable"
+read -s openID_password
+export openID_password=$openID_password
