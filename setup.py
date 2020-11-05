622492ba530860dd164c13737f67bd6fd2a3f50a
Charles Simpson <champs@bas.ac.uk>
2020-11-05 16:02:03 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+        name="src",
+        version="0.0.1",
+        author="Charles Simpson",
+        author_email="champs@bas.ac.uk",
+        description="Estimating the effect of occupational heat exposure on rice harvesting",
+        url="https://github.com/C-H-Simpson/HarvestOccupationalHeat",
+        platforms=['Unix'],
+        packages=find_packages(),
+)
