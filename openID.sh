c121ff55a6a035efe8483ea0b473a0315b337794
Charles Simpson <champs@bas.ac.uk>
2021-01-21 13:31:13 +0000

    remove openDAP code

--- openID.sh
+++
@@ -1,8 +0,0 @@
-echo "Enter openID"
-read openID
-export openID=$openID
-
-echo "Enter openID password"
-echo "This will be stored as an environment variable"
-read -s openID_password
-export openID_password=$openID_password
